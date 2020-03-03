7f30445cfda2209eb41e1bc08cb17100992c169e
tech-dona <tech_dona@outlook.jp>
2020-03-03 10:21:56 +0900

    Initial Commit

---
+++ dev-testTests/dev_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  dev_testTests.swift
+//  dev-testTests
+//
+//  Created by donadona on 2020/03/02.
+//  Copyright © 2020 lalala. All rights reserved.
+//
+
+import XCTest
+@testable import dev_test
+
+class dev_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
